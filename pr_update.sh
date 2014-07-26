c09574364bfb544c95d799ad254523d605aa44e2
Alex <joeblack78006@yahoo.com>
2014-07-26 09:37:57 -0700

    PR script successfully checks diff

--- pr_update.sh
+++ pr_update.sh
@@ -6,6 +6,11 @@ then
 	curl --silent http://www.mccain.senate.gov/public/index.cfm/press-releases >> oldFile
 fi
 
-#curl http://www.mccain.senate.gov/public/index.cfm/press-releases >> newFile
-#$UPDATED=`diff oldFile newFile`
-
+curl --silent http://www.mccain.senate.gov/public/index.cfm/press-releases >> newFile
+UPDATED=`diff oldFile newFile`
+if [[ -z $UPDATED ]]
+then
+	echo "No updates."
+else
+	echo "Updates available."
+fi
