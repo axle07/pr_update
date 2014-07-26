1ff3351f7f80ce75477f9b3d25d3e6ea054caca2
Alex <joeblack78006@yahoo.com>
2014-07-26 08:45:28 -0700

    Initial commit

---
+++ pr_update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -f ./oldFile ]
+then
+	echo "I do not have an existing copy of the PR page to compare to today's version. I will save today's now."
+	curl --silent http://www.mccain.senate.gov/public/index.cfm/press-releases >> oldFile
+fi
+
+#curl http://www.mccain.senate.gov/public/index.cfm/press-releases >> newFile
+#$UPDATED=`diff oldFile newFile`
+
