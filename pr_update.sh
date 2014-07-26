eb592f2e951dd61435bbd6f155992d43a9a574fa
Alex <joeblack78006@yahoo.com>
2014-07-26 16:13:10 -0700

    Finally working

--- pr_update.sh
+++ pr_update.sh
@@ -1,22 +1,26 @@
-#!/bin/bash
+ #!/bin/bash
+rm newFile
 
 if [ ! -f ./oldFile ]
 then
-	echo "I do not have an existing copy of the PR page to compare to today's version. I will save today's now."
-	curl --silent http://www.mccain.senate.gov/public/index.cfm/press-releases >> oldFile
+        echo "I do not have an existing copy of the PR page to compare to today's version. I will save today's now."
+        curl --silent http://www.mccain.senate.gov/public/index.cfm/press-releases > oldFile
+        exit 0
 fi
 
-curl --silent http://www.mccain.senate.gov/public/index.cfm/press-releases >> newFile
+curl --silent http://www.mccain.senate.gov/public/index.cfm/press-releases > newFile
 UPDATED=`diff oldFile newFile`
 
 if [[ -z $UPDATED ]]
 then
-	echo "No updates."
-	exit
+        echo "No updates."
+        exit 0
 else
-	echo "Updates available:"
-	UPDATEURL=`grep -o -m 1 "<a\ href=\"http://www.mccain.senate.gov\/.*class=\"ContentGrid.*a>" newFile | cut -d '"' -f2` 
-	#UPDATEURL=`grep -o "href\".*\"" $UPDATEURL`
-	echo $UPDATEURL
+        echo "Updates available:"
+        UPDATEURL=`grep -o -m 1 "<a\ href=\"http://www.mccain.senate.gov\/.*class=\"ContentGrid.*a>" newFile | cut -d '"' -f2`
+        curl --silent $UPDATEURL > update.html
+        NEWPOST=`awk '/<\/article>/{f=0} f;/<div class="header">/{f=1}' update.html`
+        echo $NEWPOST
+        cat newFile > oldFile
+        exit 0
 fi
-
